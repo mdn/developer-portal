7b1edfb35586736559d73b415304f4595a816b94
Liam Newmarch <liam@potatolondon.com>
2019-05-10 19:07:22 +0100

    Share env variables with django settings #9
    - Moved env vars to a .env file.
    - Added django-dotenv to load .env into Django.
    - Pinned versions in requirements.txt.

--- developerportal/settings/base.py
+++ developerportal/settings/base.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'developerportal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'PORT': 5432,
     }
 }
 
