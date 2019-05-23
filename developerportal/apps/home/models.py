cecd12fa63f6dea0035018103f31fa3b93f0662c
Liam Newmarch <liam@potatolondon.com>
2019-05-23 14:35:11 +0100

    Remove temp subpage_types from HomePage model #48

--- developerportal/apps/home/models.py
+++ developerportal/apps/home/models.py
@@ -2,8 +2,5 @@ from wagtail.core.models import Page
 
 
 class HomePage(Page):
-    subpage_types = [
-        'articles.Articles',
-        'topics.Topic',
-    ]
+    subpage_types = []
     template = 'home.html'
