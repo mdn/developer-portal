8621ab385792ec45628e112a064417afcde717a5
Liam Newmarch <liam@potatolondon.com>
2019-05-23 17:32:22 +0100

    Add tests for parent/sub-page types #54

---
+++ developerportal/apps/home/tests/test_home.py
@@ -0,0 +1,14 @@
+from wagtail.core.models import Page
+from wagtail.tests.utils import WagtailPageTests
+
+from ..models import HomePage
+
+
+class HomePageTests(WagtailPageTests):
+    """Tests for the HomePage model."""
+
+    def test_topic_parent_pages(self):
+        self.assertAllowedParentPageTypes(HomePage, {Page})
+
+    def test_home_page_subpages(self):
+        self.assertAllowedSubpageTypes(HomePage, {})
