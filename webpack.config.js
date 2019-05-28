ce031e54d50ce5c7064da3439c79f49c8c4ba94d
David Gauld <davidgauld@potatolondon.com>
2019-05-28 11:55:44 +0100

    Use Webpack to better support Sass watching

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const { resolve } = require('path');
+const autoprefixer = require('autoprefixer');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/js/index.js',
+  target: 'web',
+  output: {
+    filename: 'js/bundle.js',
+    path: resolve(__dirname, 'dist'),
+  },
+  mode: process.env.NODE_ENV,
+  module: {
+    rules: [
+      { test: /\.js$/, loader: 'buble-loader', exclude: /node_modules/, options: { objectAssign: 'Object.assign' } },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: { plugins: [autoprefixer] },
+          },
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/bundle.css',
+    }),
+  ],
+};
