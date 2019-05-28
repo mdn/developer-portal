21a9c5933b0dead4325b2a8cd25e027b65e23f77
Gary Homewood <gary@potatolondon.com>
2019-05-28 09:56:24 +0100

    Render nav links from CMS

--- developerportal/settings/base.py
+++ developerportal/settings/base.py
@@ -91,6 +91,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries': {
+                'page_queries': 'developerportal.templatetags.page_queries',
+            }
         },
     },
 ]
