aecbf33f7b0e1de7bdefa5f8e9f5d76804cf18b6
Gary Homewood <gary@potatolondon.com>
2019-06-11 09:56:08 +0100

    WIP: add code snippet block

---
+++ developerportal/apps/common/blocks.py
@@ -0,0 +1,14 @@
+from wagtail.core import blocks
+
+class CodeSnippetBlock(blocks.StructBlock):
+    language = blocks.ChoiceBlock(choices=[
+      ('python', 'Python'),
+      ('rust', 'Rust'),
+      ('go', 'Go'),
+      ('haskell', 'Haskell'),
+    ])
+    code = blocks.TextBlock()
+
+    class Meta:
+        template = 'code_snippet_block.html'
+
