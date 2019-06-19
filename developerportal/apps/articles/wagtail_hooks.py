267b928f86d2ad9b9837a1224dc7825c0dfc5087
Matthew Goodman <matt.goodman>
2019-06-19 14:34:40 +0100

    add authors columns

--- developerportal/apps/articles/wagtail_hooks.py
+++ developerportal/apps/articles/wagtail_hooks.py
@@ -8,9 +8,12 @@ class ArticleAdmin(ViewModelAdmin):
     model = Article
     menu_icon = 'doc-full'
     menu_order = 200
-    list_display = ('title', 'date')
+    list_display = ('title', 'authors','date')
     list_filter = ('date',)
     list_sort = ('title', 'date')
 
+    def authors(self, obj):
+        article_authors = obj.authors.all()
+        return [author.author for author in article_authors]
 
 modeladmin_register(ArticleAdmin)
