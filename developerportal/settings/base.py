6f0f66e21da818fe38b1dcd42a95fc8419d379b8
Liam Newmarch <liam@potatolondon.com>
2019-06-25 12:10:06 +0100

    Admin branding and favicon #94

--- developerportal/settings/base.py
+++ developerportal/settings/base.py
@@ -182,20 +182,18 @@ MEDIA_URL = '/media/'
 
 # Wagtail settings
 
-WAGTAIL_SITE_NAME = 'developerportal'
-
-codepen = {
-    'endpoint': 'http://codepen.io/api/oembed',
-    'urls': [
-        '^http(?:s)?://codepen\\.io/.+/pen/.+$',
-    ],
-}
+WAGTAIL_SITE_NAME = 'Mozilla Developer Portal'
 
 # Add support for CodePen oEmbed
 WAGTAILEMBEDS_FINDERS = [
     {
         'class': 'wagtail.embeds.finders.oembed',
-        'providers': [codepen],
+        'providers': [{
+            'endpoint': 'http://codepen.io/api/oembed',
+            'urls': [
+                '^http(?:s)?://codepen\\.io/.+/pen/.+$',
+            ],
+        }],
     }
 ]
 
