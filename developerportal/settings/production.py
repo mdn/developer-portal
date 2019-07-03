03f06aca1ee9356e51ddac52f0e87123739717fb
Liam Newmarch <liam@potatolondon.com>
2019-07-03 12:29:41 +0100

    Remove unnecessary dependency django-dotenv.

--- developerportal/settings/production.py
+++ developerportal/settings/production.py
@@ -3,6 +3,7 @@ from urllib.parse import urlparse
 from .base import *
 
 DEBUG = False
+DEBUG_PROPAGATE_EXCEPTIONS = True
 ALLOWED_HOSTS = [urlparse(BASE_URL).hostname]
 
 try:
--- developerportal/wsgi.py
+++ developerportal/wsgi.py
@@ -9,11 +9,7 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 
-import dotenv
 from django.core.wsgi import get_wsgi_application
 
-dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "developerportal.settings.dev")
-
 application = get_wsgi_application()
