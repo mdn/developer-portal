f2a3dad80cc74fd0a03dcc29565c888a4030fd29
Liam Newmarch <liam@potatolondon.com>
2019-07-16 08:56:47 +0100

    Add model and admin view for manual static builds #66

--- developerportal/apps/staticbuild/wagtail_hooks.py
+++ developerportal/apps/staticbuild/wagtail_hooks.py
@@ -5,18 +5,30 @@ import os
 from django.conf import settings
 from django.core.management import call_command
 
+from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
 from wagtail.core.signals import page_published, page_unpublished
+
+from .models import StaticBuild
 
 
 logging.basicConfig(level=os.environ.get('LOGLEVEL', logging.INFO))
 logger = logging.getLogger(__name__)
 
 
-def _static_build_async(pipeline=settings.STATIC_BUILD_PIPELINE):
+class ArticleAdmin(ModelAdmin):
+    model = StaticBuild
+    menu_icon = 'doc-full'
+    add_to_settings_menu = True
+
+
+modeladmin_register(ArticleAdmin)
+
+
+def _static_build_async(force=False, pipeline=settings.STATIC_BUILD_PIPELINE):
     """Calls each command in the static build pipeline in turn."""
     log_prefix = 'Static build task'
     for name, command in pipeline:
-        if settings.DEBUG:
+        if settings.DEBUG and not force:
             logger.info(f'{log_prefix} ‘{name}’ skipped.')
         else:
             logger.info(f'{log_prefix} ‘{name}’ started.')
@@ -27,7 +39,7 @@ def _static_build_async(pipeline=settings.STATIC_BUILD_PIPELINE):
 def static_build(**kwargs):
     """Callback for Wagtail publish and unpublish signals."""
     # Spawn a process to do the actual build.
-    process = Process(target=_static_build_async)
+    process = Process(target=_static_build_async, kwargs=kwargs)
     process.start()
 
 
