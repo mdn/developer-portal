0b1bb72feed34d5a6d10b1d98dfaa681ab0105de
Ryan Johnson <rjohnson@mozilla.com>
2019-07-16 11:13:10 -0700

    add aws bucket config for bakery publish command (#24)

--- k8s/config/stage.sh
+++ k8s/config/stage.sh
@@ -20,3 +20,4 @@ export APP_MEMORY_LIMIT=4Gi
 export APP_MEMORY_REQUEST=2Gi
 export APP_GUNICORN_WORKERS=2
 export APP_HOST=developer-portal-stage.mdn.mozit.cloud
+export APP_AWS_BUCKET_NAME=developer-portal-stage-178589013767
