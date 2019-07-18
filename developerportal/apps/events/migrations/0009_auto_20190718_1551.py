4f7cf3034b12f7d096759e610520fe55790f33a2
Liam Newmarch <liam@potatolondon.com>
2019-07-18 18:29:03 +0100

    Adjust fixtures/migrations #248

--- developerportal/apps/events/migrations/0009_auto_20190718_1551.py
+++ developerportal/apps/events/migrations/0009_auto_20190718_1551.py
@@ -13,9 +13,4 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AlterField(
-            model_name='events',
-            name='featured_event',
-            field=wagtail.core.fields.StreamField([('event', wagtail.core.blocks.PageChooserBlock(page_type=['events.Event', 'externalcontent.ExternalEvent'], required=False)), ('external_page', wagtail.core.blocks.StructBlock([('url', wagtail.core.blocks.URLBlock()), ('title', wagtail.core.blocks.CharBlock()), ('description', wagtail.core.blocks.TextBlock(required=False)), ('image', wagtail.images.blocks.ImageChooserBlock())]))], blank=True, null=True),
-        ),
     ]
