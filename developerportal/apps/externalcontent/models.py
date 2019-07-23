54c64d2eee14e25b609c81ca785df197e0bfd89d
Liam Newmarch <liam@potatolondon.com>
2019-07-23 17:39:53 +0100

    Reorder existing fields #248

--- developerportal/apps/externalcontent/models.py
+++ developerportal/apps/externalcontent/models.py
@@ -1,9 +1,12 @@
 import datetime
 
-from django.db.models import CharField, DateField, ForeignKey, SET_NULL, TextField, URLField
+from django.db.models import CASCADE, CharField, DateField, ForeignKey, SET_NULL, TextField, URLField
+
+from modelcluster.fields import ParentalKey
+from modelcluster.contrib.taggit import ClusterTaggableManager
 
 from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, ObjectList, TabbedInterface
-from wagtail.core.models import Page
+from wagtail.core.models import Orderable, Page
 from wagtail.images.edit_handlers import ImageChooserPanel
 
 
@@ -11,6 +14,8 @@ class ExternalContent(Page):
     is_external = True
     subpage_types = []
 
+    # Card fields
+    description = TextField(max_length=250, blank=True, default='')
     external_url = URLField('URL', max_length=2048, blank=True, default='')
     image = ForeignKey(
         'mozimages.MozImage',
@@ -20,13 +25,14 @@ class ExternalContent(Page):
         related_name='+'
     )
 
-    content_panels = Page.content_panels + [
+    card_panels = Page.content_panels + [
+        FieldPanel('description'),
         FieldPanel('external_url'),
         ImageChooserPanel('image'),
     ]
 
     edit_handler = TabbedInterface([
-        ObjectList(content_panels, heading='Content'),
+        ObjectList(card_panels, heading='Card'),
         ObjectList(Page.settings_panels, heading='Settings', classname='settings'),
     ])
 
@@ -47,12 +53,16 @@ class ExternalContent(Page):
 class ExternalArticle(ExternalContent):
     read_time = CharField(max_length=30, blank=True)
 
-    content_panels = ExternalContent.content_panels + [
+    card_panels = ExternalContent.card_panels + [
+    ]
+
+    meta_panels = [
         FieldPanel('read_time'),
     ]
 
     edit_handler = TabbedInterface([
-        ObjectList(content_panels, heading='Content'),
+        ObjectList(card_panels, heading='Card'),
+        ObjectList(meta_panels, heading='Meta'),
         ObjectList(Page.settings_panels, heading='Settings', classname='settings'),
     ])
 
@@ -62,7 +72,10 @@ class ExternalEvent(ExternalContent):
     end_date = DateField(blank=True, null=True)
     venue = TextField(max_length=250, blank=True, default='')
 
-    content_panels = ExternalContent.content_panels + [
+    card_panels = ExternalContent.card_panels + [
+    ]
+
+    meta_panels = [
         MultiFieldPanel([
             FieldPanel('start_date'),
             FieldPanel('end_date'),
@@ -71,7 +84,8 @@ class ExternalEvent(ExternalContent):
     ]
 
     edit_handler = TabbedInterface([
-        ObjectList(content_panels, heading='Content'),
+        ObjectList(card_panels, heading='Card'),
+        ObjectList(meta_panels, heading='Meta'),
         ObjectList(Page.settings_panels, heading='Settings', classname='settings'),
     ])
 
@@ -79,11 +93,15 @@ class ExternalEvent(ExternalContent):
 class ExternalVideo(ExternalContent):
     video_duration = CharField(max_length=30, blank=True, default='0:00')
 
-    content_panels = ExternalContent.content_panels + [
+    card_panels = ExternalContent.card_panels + [
+    ]
+
+    meta_panels = [
         FieldPanel('video_duration'),
     ]
 
     edit_handler = TabbedInterface([
-        ObjectList(content_panels, heading='Content'),
+        ObjectList(card_panels, heading='Card'),
+        ObjectList(meta_panels, heading='Meta'),
         ObjectList(Page.settings_panels, heading='Settings', classname='settings'),
     ])
