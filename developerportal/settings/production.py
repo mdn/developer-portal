97ad2a9b9ea5a8d47da0b4d63ec832558d268645
Ed Lim <limed@mozilla.com>
2019-08-02 09:53:49 -0700

    Reverting c594f31c999088f28a2caf66730915f852428cab (#53)

--- developerportal/settings/production.py
+++ developerportal/settings/production.py
@@ -5,7 +5,6 @@ from .base import *
 DEBUG = False
 DEBUG_PROPAGATE_EXCEPTIONS = True
 ALLOWED_HOSTS = [urlparse(BASE_URL).hostname] if BASE_URL else []
-SECURE_SSL_REDIRECT = True
 
 try:
     from .local import *
