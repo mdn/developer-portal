c594f31c999088f28a2caf66730915f852428cab
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2019-08-02 17:31:39 +0100

    Add SECURE_SSL_REDIRECT to production settings (#22) (#52)

--- developerportal/settings/production.py
+++ developerportal/settings/production.py
@@ -5,6 +5,7 @@ from .base import *
 DEBUG = False
 DEBUG_PROPAGATE_EXCEPTIONS = True
 ALLOWED_HOSTS = [urlparse(BASE_URL).hostname] if BASE_URL else []
+SECURE_SSL_REDIRECT = True
 
 try:
     from .local import *
