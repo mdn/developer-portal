9260e14d26bd3ecaadd556afe7b940ed5fe80d64
Gary Homewood <gary@potatolondon.com>
2019-08-06 16:39:48 +0100

    Add latest videos for topic

--- developerportal/apps/common/utils.py
+++ developerportal/apps/common/utils.py
@@ -1,5 +1,6 @@
 from itertools import chain
 from operator import attrgetter
+from wagtail.core.models import Page
 
 
 def get_combined_articles(page, **filters):
@@ -18,3 +19,10 @@ def get_combined_events(page, **filters):
     internal = Event.objects.filter(**filters).public().live().not_page(page).distinct()
     external = ExternalEvent.objects.filter(**filters).public().live().not_page(page).distinct()
     return sorted(chain(internal, external), key=attrgetter('start_date'))
+
+def get_combined_videos(page, **filters):
+    from ..videos.models import Video
+    from ..externalcontent.models import ExternalVideo
+    internal = Video.objects.filter(**filters).public().live().not_page(page).distinct()
+    external = ExternalVideo.objects.filter(**filters).public().live().not_page(page).distinct()
+    return sorted(chain(internal, external), key=attrgetter('date'))
