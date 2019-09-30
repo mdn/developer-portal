52637cba93a72fe2b545b566fc30d40828d81529
Steve Jalim <steve@somefantastic.co.uk>
2019-09-30 09:58:47 +0100

    Hotfix: downgrade some production security settings for local development only
    These changes are needed to allow a user to log in locally on a non-HTTPS setup

--- developerportal/settings/dev.py
+++ developerportal/settings/dev.py
@@ -8,7 +8,10 @@ ALLOWED_HOSTS = ["*"]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
+# Disable production defaults that get in the way of local development
 SECURE_SSL_REDIRECT = False
+CSRF_COOKIE_SECURE = False
+SESSION_COOKIE_SECURE = False
 
 try:
     from .local import *
