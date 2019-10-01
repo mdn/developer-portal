2d8413d5ac4f960d9ba19cf9e580cd796c536f9b
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2019-10-01 14:57:29 +0100

    Refactor map embed script

--- src/js/atoms/map-embed.js
+++ src/js/atoms/map-embed.js
@@ -1,43 +1,65 @@
 /* globals mapboxgl */
+
+/**
+ * Creates a Map Box embed with marker.
+ */
 module.exports = class MapEmbed {
+  /**
+   * Returns a Map instance from options.
+   * @param {Array<number>} center
+   * @param {Element} container
+   * @param {string} style
+   * @param {number} zoom
+   * @returns {mapboxgl.Map}
+   */
+  static createMap(
+    center,
+    container,
+    style = 'mapbox://styles/mapbox/light-v10',
+    zoom = 16,
+  ) {
+    return new mapboxgl.Map({ center, container, style, zoom });
+  }
+
+  /**
+   * Returns a Marker instance and sets coordinates.
+   * @param {Array<number>} coords
+   * @returns {mapboxgl.Marker}
+   */
+  static createMarker(coords) {
+    const el = document.createElement('div');
+    el.classList.add('marker');
+    return new mapboxgl.Marker(el).setLngLat(coords);
+  }
+
+  /**
+   * Returns a Popup instance and sets its HTML.
+   * @param {string} content
+   * @returns {mapboxgl.Popup}
+   */
+  static createPopup(content) {
+    return new mapboxgl.Popup({ offset: 25 }).setHTML(`<p>${content}</p>`);
+  }
+
+  /**
+   * Constructs an instance of MapEmbed class for each map element.
+   */
   static init() {
     const maps = Array.from(document.querySelectorAll('.map'));
-    maps.forEach(mapEl => {
-      const map = new mapboxgl.Map({
-        container: mapEl.dataset.mapId,
-        style: 'mapbox://styles/mapbox/light-v10',
-        center: [mapEl.dataset.lng, mapEl.dataset.lat],
-        zoom: 16,
-      });
+    maps.forEach(el => new MapEmbed(el));
+  }
 
-      const geojson = {
-        type: 'FeatureCollection',
-        features: [
-          {
-            type: 'Feature',
-            geometry: {
-              type: 'Point',
-              coordinates: [mapEl.dataset.lng, mapEl.dataset.lat],
-            },
-            properties: {
-              venue: mapEl.dataset.venue,
-            },
-          },
-        ],
-      };
-
-      geojson.features.forEach(marker => {
-        const el = document.createElement('div');
-        el.className = 'marker';
-        new mapboxgl.Marker(el)
-          .setLngLat(marker.geometry.coordinates)
-          .setPopup(
-            new mapboxgl.Popup({ offset: 25 }).setHTML(
-              `<p>${marker.properties.venue}</p>`,
-            ),
-          )
-          .addTo(map);
-      });
-    });
+  /**
+   * Creates a Map, Marker and Popup based on the element’s data attributes.
+   * @param {Element} el
+   */
+  constructor(el) {
+    const { lat, lng, mapId, venue } = el.dataset;
+    const coords = [lng, lat];
+    const map = MapEmbed.createMap(coords, mapId);
+    const popup = MapEmbed.createPopup(venue);
+    MapEmbed.createMarker(coords)
+      .setPopup(popup)
+      .addTo(map);
   }
 };
