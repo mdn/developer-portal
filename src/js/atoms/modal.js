66949c2813b7a121b828ee10587e7a9a7c36241b
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2019-10-02 15:25:02 +0100

    Generate embed URL and construct modal content dynamically

--- src/js/atoms/modal.js
+++ src/js/atoms/modal.js
@@ -1,36 +1,92 @@
+// Transforms a normal YouTube URL into an iframe embed URL
+function transformYouTubeURL(url) {
+  const re = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+  const match = url.match(re);
+  if (!match) throw new Error('Failed to match YouTube ID');
+  const id = match[2];
+  if (id.length !== 11) throw new Error(`Invalid YouTube ID ${id}`);
+  return `https://www.youtube.com/embed/${id}?autoplay=1&enablejsapi=1&modestbranding=1&rel=0`;
+}
+
 module.exports = class Modal {
-  static createModalOptions({
-    dataset: {
-      className = 'mzp-has-media',
-      closeText = 'Close modal',
-      title = 'Example headline with 35 characters',
-    } = {},
-  } = {}) {
-    return { className, closeText, title };
+  static create(trigger) {
+    const { content, ...options } = this.createModalOptions(trigger);
+    const element = document.createElement('div');
+    element.className = 'mzp-u-modal-content';
+    element.appendChild(content);
+    return {
+      open() {
+        Mzp.Modal.createModal(element, content, {
+          ...options,
+          // The following methods prevent the video from autoplaying when the
+          // modal is closed in some browsers.
+          onCreate() {
+            document.body.appendChild(element);
+          },
+          onDestroy() {
+            document.body.removeChild(element);
+          },
+        });
+      },
+    };
+  }
+
+  static createModalContent(element) {
+    const content = document.createElement('div');
+    switch (element.dataset.type) {
+      // Handle more content types here.
+      case 'video':
+        content.appendChild(this.createModalContentVideo(element));
+        break;
+      default:
+        throw new Error(`Unhandled type '${element.dataset.type}'`);
+    }
+    return content;
+  }
+
+  static createModalContentVideo(element) {
+    const container = document.createElement('div');
+    container.className = 'responsive-object';
+    container.style.paddingBottom = '56.25%';
+    const iframe = document.createElement('iframe');
+    iframe.allowFullscreen = true;
+    iframe.frameBorder = 0;
+    iframe.src = transformYouTubeURL(element.href);
+    iframe.setAttribute(
+      'allow',
+      'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture',
+    );
+    container.appendChild(iframe);
+    return container;
+  }
+
+  static createModalOptions(element) {
+    const content = this.createModalContent(element);
+    const { className, closeText = 'Close modal', title } = element.dataset;
+    return { className, closeText, content, title };
   }
 
   static init() {
-    const contentEls = document.querySelectorAll('.mzp-u-modal-content');
-    Array.from(contentEls).forEach(content => new Modal(content));
+    const elements = document.querySelectorAll('.js-modal-trigger');
+    Array.from(elements).forEach(content => new Modal(content));
   }
 
-  constructor(content) {
-    this.content = content;
-    this.bindListeners();
+  constructor(trigger) {
+    this.trigger = trigger;
+    this.trigger.addEventListener('click', event => this.open(event));
   }
 
-  bindListeners() {
-    const selector = `.js-modal-trigger[data-modal=${this.content.id}]`;
-    const modalTriggers = document.querySelectorAll(selector);
-    Array.from(modalTriggers).forEach(modalTrigger => {
-      modalTrigger.addEventListener('click', event => this.toggle(event));
-    });
+  open(event) {
+    try {
+      if (!('modal' in this)) {
+        this.modal = Modal.create(this.trigger);
+      }
+      // Prevent navigation only if a modal has been created
+      event.preventDefault();
+      this.modal.open();
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.warn(`Error creating modal: ${error.message}`);
+    }
   }
-
-  toggle(event) {
-    event.preventDefault();
-    const target = event.currentTarget || event.target;
-    const options = Modal.createModalOptions(target);
-    Mzp.Modal.createModal(target, this.content, options);
-  }
-}
+};
