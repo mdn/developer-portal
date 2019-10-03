1baefd339156d7017a3e1d98076bf0649dd16b41
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2019-10-03 10:21:49 +0100

    Clean up modal JS, add comments

--- src/js/atoms/modal.js
+++ src/js/atoms/modal.js
@@ -1,36 +1,51 @@
-// Transforms a normal YouTube URL into an iframe embed URL
+/**
+ * @constant
+ * @type {RegExp}
+    ^https?:\/\/              Start with 'http://' or 'https://'
+    (?:                       non-capturing group matching
+      (?:www\.)?                an optional 'www.' subdomain
+      youtube(?:-nocookie)?     'youtube' or 'youtube-nocookie' domain
+      \.com\/                   '.com/' TLD (explicit slash to prevent matching 'https://youtube.com.foo/')
+      .*                        any characters
+      (?:                       non-capturing group matching
+        \/|                       '/' or
+        v\/|                      'v/' or
+        u\/\w\/|                  'u/w/' or
+        embed\/|                  'embed/' or
+        \?v=|                     '?v=' or
+        &v=                       '&v='
+      )                         end non-capturing group
+    |                         or
+      youtu.be\/                'youtu.be/' domain and TLD (explicit slash to prevent matching 'https://youtu.be.foo/')
+    )                         end non-capturing group
+    (?                        a capturing group matching
+      [\w-]{11}                 11 characters matching A-Z, a-z, 0-9, hyphen or underscore
+    )                         end capture group
+ */
+const RE_MATCH_YOUTUBE_URL = /^https?:\/\/(?:(?:www\.)?youtube(?:-nocookie)?\.com\/.*(?:\/|v\/|u\/\w\/|embed\/|\?v=|&v=)|youtu.be\/)([\w-]{11})/;
+
+/**
+ * Helper method, transforms a normal YouTube URL into an iframe embed URL
+ * @param {string} url A YouTube URL string
+ * @returns {string}
+ */
 function transformYouTubeURL(url) {
-  const re = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
-  const match = url.match(re);
+  const match = url.match(RE_MATCH_YOUTUBE_URL);
   if (!match) throw new Error('Failed to match YouTube ID');
-  const id = match[2];
-  if (id.length !== 11) throw new Error(`Invalid YouTube ID ${id}`);
+  const id = match[1];
   return `https://www.youtube.com/embed/${id}?autoplay=1&enablejsapi=1&modestbranding=1&rel=0`;
 }
 
+/**
+ * Listens to a trigger element for click events and constructs content for use
+ * with Mzp.Modal.
+ */
 module.exports = class Modal {
-  static create(trigger) {
-    const { content, ...options } = this.createModalOptions(trigger);
-    const element = document.createElement('div');
-    element.className = 'mzp-u-modal-content';
-    element.appendChild(content);
-    return {
-      open() {
-        Mzp.Modal.createModal(element, content, {
-          ...options,
-          // The following methods prevent the video from autoplaying when the
-          // modal is closed in some browsers.
-          onCreate() {
-            document.body.appendChild(element);
-          },
-          onDestroy() {
-            document.body.removeChild(element);
-          },
-        });
-      },
-    };
-  }
-
+  /**
+   * Returns modal content for given trigger element, via delegation.
+   * @param {Element} element
+   * @returns {Element}
+   */
   static createModalContent(element) {
     const content = document.createElement('div');
     switch (element.dataset.type) {
@@ -44,6 +59,11 @@ module.exports = class Modal {
     return content;
   }
 
+  /**
+   * Returns modal content for 'video' type trigger elements.
+   * @param {Element} element
+   * @returns {Element}
+   */
   static createModalContentVideo(element) {
     const container = document.createElement('div');
     container.className = 'responsive-object';
@@ -60,30 +80,73 @@ module.exports = class Modal {
     return container;
   }
 
+  /**
+   * Returns Mzp.Modal options from an element’s data attributes.
+   * @param {Element} element
+   * @returns {object}
+   */
   static createModalOptions(element) {
     const content = this.createModalContent(element);
     const { className, closeText = 'Close modal', title } = element.dataset;
     return { className, closeText, content, title };
   }
 
+  /**
+   * Constructs an instance of Modal class for each trigger element.
+   */
   static init() {
     const elements = document.querySelectorAll('.js-modal-trigger');
     Array.from(elements).forEach(content => new Modal(content));
   }
 
+  /**
+   * Binds click event listener to trigger element.
+   * @param {Element} trigger
+   */
   constructor(trigger) {
     this.trigger = trigger;
-    this.trigger.addEventListener('click', event => this.open(event));
+    this.trigger.addEventListener('click', event => this.onClick(event));
   }
 
-  open(event) {
+  /**
+   * Initial set up for modal, called on demand instead of in constructor.
+   */
+  createModal() {
+    const { content, ...options } = Modal.createModalOptions(this.trigger);
+    this.content = content;
+    this.options = options;
+    this.modal = document.createElement('div');
+    this.modal.className = 'mzp-u-modal-content';
+    this.modal.appendChild(content);
+  }
+
+  /**
+   * Creates the Mzp.Modal instance.
+   */
+  open() {
+    Mzp.Modal.createModal(this.modal, this.content, {
+      ...this.options,
+      // The following methods prevent the video from autoplaying when the
+      // modal is closed in some browsers.
+      onCreate() {
+        document.body.appendChild(this.modal);
+      },
+      onDestroy() {
+        document.body.removeChild(this.modal);
+      },
+    });
+  }
+
+  /**
+   * Creates a modal if one does not exist and opens it.
+   * @param {Event} event
+   */
+  onClick(event) {
     try {
-      if (!('modal' in this)) {
-        this.modal = Modal.create(this.trigger);
-      }
+      if (!this.modal) this.createModal();
       // Prevent navigation only if a modal has been created
       event.preventDefault();
-      this.modal.open();
+      this.open();
     } catch (error) {
       // eslint-disable-next-line no-console
       console.warn(`Error creating modal: ${error.message}`);
