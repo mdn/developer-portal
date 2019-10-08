8e9dc1fa3ccc9aa27fbb110384197df2e1096f60
Steve Jalim <steve@somefantastic.co.uk>
2019-10-08 21:23:28 +0100

    139 fix auto creating of static build (#325)
    * Initial steps moving automatic generation and upload of the static site to a queue-managed background task.
    
    Changes include:
    * Update Dockerfile to:
      - rename `app` target as `app_base`, to make it clearer it's being used for multiple things
      - not start gunicorn as part of `app_base`'s build, because that's not needed and is handled by docker-compose or k8s
    
    * Update docker-compose.yml to:
      - add `redis` container
      - update the `app` config to inherit from a new `common` config, which uses the `app_base` target
      - add new `worker` container, inheriting from the `common` config, too
    
    * Add `celery` to the project
      - Stop the use of `subprocess.Process` when a page is published or unpublished.
      - Use a a properly enqueued task to handle the background dump-to-static-then-upload-to-S3 process
    
    * Add django-celery-results to give us easier visibility of job success/failure
    
    This is a temporary and basic solution to give us an eye on whether background tasks are
    happy, but it is limited because it only displays tasks after they have completed (ie
    once they have a result)
    
    Switching to something that lets us see in-flight tasks would be useful, down the line.
    (eg: Flower - https://flower.readthedocs.io/en/latest/)
    
    * Nit: Be explicit about which Redis DB to use
    
    * Ensure Django (and therefore the Celery worker) can get hold of the credentials Celery needs
    
    - Update Makefile to export AWS-related config to the environment
    - In Django, pick up AWS-related config from environment
    
    This change assumes the AWS-related config is already available at in the general config
    at the k8s level -- this seems reasonable, because they are all already mentioned in the
    k8s config files.
    
    * Nit: tidy up output of async logging
    
    * Add test settings to run tests using in-memory SQLite, optionally
    
    * Add light integration tests to show that publishing and unpublishing will trigger static-build-and-sync behaviour
    
    * 139: Remove unnecessary redelcaration of AWS vars (following CR from @limed)
    
    * Update isort config to be aware of celery as as a known third-party
    
    * 139: Update docker config to create a non-root user to use in operations
    
    * Update Dockerfile to add a new user "devportaluser" with a fixed UID, NO PASSWORD and a home dir
    * Make them the owner of the mounted /app/ dir
    
    Update docker-compose to run gunicorn in `app` and Celery in `worker` using "devportaluser". This silences "RuntimeWarning: You're running the worker with superuser privileges: this is absolutely not recommended!" from Celery
    
    --
    Manually checked that
    - `collectstatic` still works
    - gunicorn/site serving still works
    - wagtail-bakery's `build` and `publish` steps still work
    
    ```
    $ docker-compose exec app /bin/sh
    /app $ whoami
    devportaluser
    
    /app $ rm -rf static/*
    /app $ python manage.py collectstatic
    
    586 static files copied to '/app/static', 1176 post-processed.
    
    /app $ ./manage.py build
    INFO:bakery.management.commands.build:Build started
    INFO:bakery.management.commands.build:Build finished
    /app $ ./manage.py publish
    Uploading /app/build/index.html
    ... [etc etc] ...
    ```

--- developerportal/apps/staticbuild/wagtail_hooks.py
+++ developerportal/apps/staticbuild/wagtail_hooks.py
@@ -1,12 +1,13 @@
 import logging
 import os
-from multiprocessing import Process
 
 from django.conf import settings
 from django.core.management import call_command
 
 from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
 from wagtail.core.signals import page_published, page_unpublished
+
+from developerportal.apps.staticbuild.celery import app
 
 from .models import StaticBuild
 
@@ -23,23 +24,24 @@ class ArticleAdmin(ModelAdmin):
 modeladmin_register(ArticleAdmin)
 
 
-def _static_build_async(force=False, pipeline=settings.STATIC_BUILD_PIPELINE, **kwargs):
+@app.task
+def _static_build_async(force=False, pipeline=settings.STATIC_BUILD_PIPELINE):
     """Calls each command in the static build pipeline in turn."""
     log_prefix = "Static build task"
     for name, command in pipeline:
         if settings.DEBUG and not force:
-            logger.info(f"{log_prefix} ‘{name}’ skipped.")
+            logger.info(f"{log_prefix} (wagtail-bakery) command '{name}' skipped.")
         else:
-            logger.info(f"{log_prefix} ‘{name}’ started.")
+            logger.info(f"{log_prefix} (wagtail-bakery) command '{name}' started.")
             call_command(command)
-            logger.info(f"{log_prefix} ‘{name}’ finished.")
+            logger.info(f"{log_prefix} (wagtail-bakery) command '{name}' finished.")
 
 
 def static_build(**kwargs):
-    """Callback for Wagtail publish and unpublish signals."""
-    # Spawn a process to do the actual build.
-    process = Process(target=_static_build_async, kwargs=kwargs)
-    process.start()
+    """Callback for Wagtail publish and unpublish signals to spawn a
+    task-queue job to do the actual build"""
+    force = kwargs.get("force", False)
+    _static_build_async.delay(force=force)
 
 
 page_published.connect(static_build)
