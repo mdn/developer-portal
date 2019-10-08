9807ef069c5f03fc1345bce4d9117100db8c7b6a
David Gauld <davidgauld@potatolondon.com>
2019-10-08 14:11:40 +0100

    137: Prevent past events from showing in resource lists (#268)
    * Ensure no past events are returned in event lists
    
    * Move resource list checks into filter component
    
    * Add test for Events.events

--- developerportal/apps/events/tests/test_events.py
+++ developerportal/apps/events/tests/test_events.py
@@ -1,3 +1,5 @@
+import datetime
+
 from wagtail.tests.utils import WagtailPageTests
 
 from ...home.models import HomePage
@@ -17,8 +19,38 @@ class EventTests(WagtailPageTests):
 class EventsTests(WagtailPageTests):
     """Tests for the Events model."""
 
+    fixtures = ["common.json"]
+
     def test_events_parent_pages(self):
         self.assertAllowedParentPageTypes(Events, {HomePage})
 
     def test_events_subpages(self):
         self.assertAllowedSubpageTypes(Events, {Event})
+
+    def test_events_upcoming_events(self):
+        events_page = Events.published_objects.first()
+        now = datetime.datetime.now()
+
+        event_today = Event(depth=2, path="00019998", start_date=now, title="Today")
+        event_today.save()
+
+        event_tomorrow = Event(
+            depth=2,
+            path="00019999",
+            start_date=now + datetime.timedelta(days=1),
+            title="Tomorrow",
+        )
+        event_tomorrow.save()
+
+        event_yesterday = Event(
+            depth=2,
+            path="00019997",
+            start_date=now + datetime.timedelta(days=-1),
+            title="Yesterday",
+        )
+        event_yesterday.save()
+
+        events = events_page.events
+        self.assertIn(event_today, events)
+        self.assertIn(event_tomorrow, events)
+        self.assertNotIn(event_yesterday, events)
