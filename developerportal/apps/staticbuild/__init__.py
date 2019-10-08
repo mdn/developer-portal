d85525c4ce8d8fc5f5c8dd8537db86442d7fd32b
Steve Jalim <steve@somefantastic.co.uk>
2019-10-08 11:34:26 +0100

    Revert "139 fix auto creating of static build (#312)"
    This reverts commit e502e3f381e61c9121c6bf99cc538ff010ce7c4f.

--- developerportal/apps/staticbuild/__init__.py
+++
@@ -1,5 +0,0 @@
-from .celery import app as celery_app
-
-# Make sure Celery is always imported when Django starts
-# so that @shared_task will use this app.
-__all__ = ("celery_app",)
--- developerportal/settings/test_fast.py
+++
@@ -1,3 +0,0 @@
-from .dev import *
-
-DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}}
