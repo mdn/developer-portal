12798b1f953777228f25ceb21ce47f08a521b640
Steve Jalim <steve@somefantastic.co.uk>
2019-10-16 20:50:35 +0100

    Amend management command to call a more appropriate build-and-sync command ...so that we don't risk concurrent builds

--- developerportal/apps/staticbuild/management/commands/enqueue_build_and_sync_task.py
+++ developerportal/apps/staticbuild/management/commands/enqueue_build_and_sync_task.py
@@ -2,16 +2,16 @@ import logging
 
 from django.core.management.base import BaseCommand
 
-from developerportal.apps.staticbuild.wagtail_hooks import _static_build_async
+from developerportal.apps.staticbuild.wagtail_hooks import static_build
 
 logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
-    """Management to manually queue up a "build-and-sync-to-S3" of the
-    current published state of the site"""
+    """Management to manually enqueue a request for a "build-and-sync-to-S3" of the
+    current/latest published state of the site."""
 
     def handle(self, *args, **options):
-        logger.info("Adding a build-and-sync request to the task queue.")
-        _static_build_async.delay()
-        logger.info("Build-and-sync added to task queue.")
+        logger.info("Adding a request for a build-and-sync request to the task queue.")
+        static_build()
+        logger.info("Build-and-sync request added to task queue.")
