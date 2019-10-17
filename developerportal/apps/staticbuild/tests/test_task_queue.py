47949a807cad6fdec4f25841a790db9b2f47de6d
Steve Jalim <steve@somefantastic.co.uk>
2019-10-17 18:26:08 +0100

    Add tests for all of the static-build tasks/funcs
    The only thing that lacks tests is .context_managers.redis_lock() - for now...

--- developerportal/apps/staticbuild/tests/test_task_queue.py
+++ developerportal/apps/staticbuild/tests/test_task_queue.py
@@ -1,17 +1,34 @@
 import datetime
 from unittest import mock
 
-from django.test import TestCase, override_settings
+from django.conf import settings
+from django.core.cache import cache
+from django.test import TestCase
 
 import pytz
 from developerportal.apps.staticbuild.wagtail_hooks import (
+    EXPECTED_BUILD_AND_SYNC_JOB_FUNC_NAME,
+    SENTINEL_KEY_NAME,
+    SENTINEL_LOCK_NAME,
     _generate_build_path,
+    _get_build_needed_sentinel,
+    _is_build_and_sync_job,
+    _request_static_build,
+    _set_build_needed_sentinel,
     _static_build_async,
+    static_build,
 )
 
 
 class TaskQueueBuildTests(TestCase):
-    @override_settings(BUILD_DIR="/path/to/build/dir/")
+    def setUp(self):
+        cache.delete(SENTINEL_KEY_NAME)
+        cache.delete(SENTINEL_LOCK_NAME)
+
+    def tearDown(self):
+        cache.delete(SENTINEL_KEY_NAME)
+        cache.delete(SENTINEL_LOCK_NAME)
+
     @mock.patch("developerportal.apps.staticbuild.wagtail_hooks.tz_now")
     def test__generate_build_path(self, mock_tz_now):
         mock_tz_now.return_value = datetime.datetime(
@@ -19,15 +36,23 @@ class TaskQueueBuildTests(TestCase):
         )
         self.assertEqual(
             _generate_build_path(),
-            "/path/to/build/dir/2001-12-25T01:23:45.123456+00:00",
+            f"{settings.BUILD_DIR}/2001-12-25T01:23:45.123456+00:00",
         )
 
     @mock.patch("developerportal.apps.staticbuild.wagtail_hooks.call_command")
     @mock.patch("developerportal.apps.staticbuild.wagtail_hooks.shutil.rmtree")
     @mock.patch("developerportal.apps.staticbuild.wagtail_hooks._generate_build_path")
-    def test_static_build_async__uses_specific_build_path(
-        self, mock_generate_build_path, mock_rmtree, mock_call_command
+    @mock.patch(
+        "developerportal.apps.staticbuild.wagtail_hooks._get_build_needed_sentinel"
+    )
+    def test__static_build_async(
+        self,
+        mock_get_build_needed_sentinel,
+        mock_generate_build_path,
+        mock_rmtree,
+        mock_call_command,
     ):
+        mock_get_build_needed_sentinel.return_value = True  # else the build is blocked
         mock_generate_build_path.return_value = "/build/subdir/"
 
         _static_build_async()
@@ -40,3 +65,123 @@ class TaskQueueBuildTests(TestCase):
         assert mock_call_command.call_args_list[1][1]["build_dir"] == "/build/subdir/"
 
         mock_rmtree.assert_called_once_with("/build/subdir/")
+
+    @mock.patch("developerportal.apps.staticbuild.wagtail_hooks.call_command")
+    @mock.patch("developerportal.apps.staticbuild.wagtail_hooks.shutil.rmtree")
+    @mock.patch("developerportal.apps.staticbuild.wagtail_hooks._generate_build_path")
+    @mock.patch(
+        "developerportal.apps.staticbuild.wagtail_hooks._get_build_needed_sentinel"
+    )
+    @mock.patch(
+        "developerportal.apps.staticbuild.wagtail_hooks._set_build_needed_sentinel"
+    )
+    def test__static_build_async__not_requested(
+        self,
+        mock_set_build_needed_sentinel,
+        mock_get_build_needed_sentinel,
+        mock_generate_build_path,
+        mock_rmtree,
+        mock_call_command,
+    ):
+        mock_get_build_needed_sentinel.return_value = False
+
+        with self.assertLogs(
+            "developerportal.apps.staticbuild.wagtail_hooks", level="INFO"
+        ) as cm:
+            _static_build_async()
+
+        self.assertEqual(
+            cm.output,
+            [
+                (
+                    "INFO:developerportal.apps.staticbuild.wagtail_hooks:"
+                    "[Static-build-and-sync] No fresh static build requested."
+                )
+            ],
+        )
+
+        assert not mock_generate_build_path.called
+        assert not mock_call_command.called
+        assert not mock_rmtree.called
+        assert not mock_set_build_needed_sentinel.called
+
+    @mock.patch("developerportal.apps.staticbuild.context_managers.cache.add")
+    @mock.patch("developerportal.apps.staticbuild.wagtail_hooks.call_command")
+    @mock.patch("developerportal.apps.staticbuild.wagtail_hooks.shutil.rmtree")
+    @mock.patch("developerportal.apps.staticbuild.wagtail_hooks._generate_build_path")
+    @mock.patch(
+        "developerportal.apps.staticbuild.wagtail_hooks._get_build_needed_sentinel"
+    )
+    @mock.patch(
+        "developerportal.apps.staticbuild.wagtail_hooks._set_build_needed_sentinel"
+    )
+    def test__static_build_async__locked_but_requested(
+        self,
+        mock_set_build_needed_sentinel,
+        mock_get_build_needed_sentinel,
+        mock_generate_build_path,
+        mock_rmtree,
+        mock_call_command,
+        mock_cache_add,
+    ):
+        mock_get_build_needed_sentinel.return_value = True  # else the build is blocked
+        mock_generate_build_path.return_value = "/build/subdir/"
+
+        mock_get_build_needed_sentinel.return_value = True  # else the build is blocked
+
+        # Necause we're faking the sentinel's presence we can also check that it's
+        # re-set by a lock-blocked _static_build_async()
+        assert cache.get(SENTINEL_KEY_NAME) is None  #  for comparison later
+
+        # Mimick the idea that the job couldn't get a lock because
+        mock_cache_add.return_value = False
+
+        assert not mock_set_build_needed_sentinel.called
+
+        _static_build_async()
+
+        assert not mock_generate_build_path.called
+        assert not mock_call_command.called
+        assert not mock_rmtree.called
+        assert mock_cache_add.call_count == 1
+
+        # The blocked job should have set a flag for a retry next CeleryBeat ping
+        assert mock_set_build_needed_sentinel.call_count == 1
+
+    def test__is_build_and_sync_job(self):
+        for config in [
+            {"name": "test", "expected_result": False},
+            {"name": EXPECTED_BUILD_AND_SYNC_JOB_FUNC_NAME, "expected_result": True},
+        ]:
+            with self.subTest(config=config):
+                job_details = {"name": config["name"]}
+                self.assertEqual(
+                    _is_build_and_sync_job(job_details), config["expected_result"]
+                )
+
+    def test__set_build_needed_sentinel(self):
+        assert cache.get(SENTINEL_KEY_NAME) is None
+        _set_build_needed_sentinel(oid="test")
+        assert cache.get(SENTINEL_KEY_NAME) is True
+
+    def test__get_build_needed_sentinel(self):
+        assert cache.get(SENTINEL_KEY_NAME) is None
+        assert _get_build_needed_sentinel(oid="test") is None
+        cache.set(SENTINEL_KEY_NAME, True)
+        assert _get_build_needed_sentinel(oid="test") is True
+
+    @mock.patch(
+        "developerportal.apps.staticbuild.wagtail_hooks._set_build_needed_sentinel"
+    )
+    def test__request_static_build(self, mock_set_build_needed_sentinel):
+        assert not mock_set_build_needed_sentinel.called
+        _request_static_build()
+        assert mock_set_build_needed_sentinel.call_count == 1
+
+    @mock.patch(
+        "developerportal.apps.staticbuild.wagtail_hooks._request_static_build.delay"
+    )
+    def test_static_build(self, mock_request_static_build_delay):
+        assert not mock_request_static_build_delay.called
+        static_build()
+        assert mock_request_static_build_delay.call_count == 1
