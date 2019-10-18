646d7e5c4a884c9110c1820c66ef6764c7a980ab
Steve Jalim <steve@somefantastic.co.uk>
2019-10-18 11:51:58 +0100

    Fixup test regressions in this changeset

--- developerportal/apps/staticbuild/tests/test_auto_build.py
+++ developerportal/apps/staticbuild/tests/test_auto_build.py
@@ -1,41 +1,31 @@
 from unittest import mock
 
-from django.test import TestCase, override_settings
+from django.test import TestCase
 
 from developerportal.apps.home.models import HomePage
 
 
-@override_settings(CELERY_ALWAYS_EAGER=True)
 class TestAutomaticBakingUponPublish(TestCase):
-    @mock.patch("developerportal.apps.staticbuild.wagtail_hooks.call_command")
-    def test_page_publish_triggers_async_tasks(self, mock_call_command):
+    @mock.patch(
+        "developerportal.apps.staticbuild.wagtail_hooks._request_static_build.delay"
+    )
+    def test_page_publish_triggers_async_tasks(self, mock_request_static_build_delay):
         self.homepage = HomePage.objects.get()
 
-        mock_call_command.reset_mock()
+        assert not mock_request_static_build_delay.called
 
         # One doesn't publish a page, one publishes a Revision
         revision = self.homepage.save_revision()
         revision.publish()
+        assert mock_request_static_build_delay.call_count == 1
 
-        assert mock_call_command.call_count == 2
-
-        #  check for the call that builds the static site
-        assert mock_call_command.call_args_list[0][0][0] == "build"
-
-        # now check for the call to sync the static site to S3
-        assert mock_call_command.call_args_list[1][0][0] == "publish"
-
-    @mock.patch("developerportal.apps.staticbuild.wagtail_hooks.call_command")
-    def test_page_unpublish_triggers_async_tasks(self, mock_call_command):
+    @mock.patch(
+        "developerportal.apps.staticbuild.wagtail_hooks._request_static_build.delay"
+    )
+    def test_page_unpublish_triggers_async_tasks(self, mock_request_static_build_delay):
         self.homepage = HomePage.objects.get()
-        mock_call_command.reset_mock()
+        assert not mock_request_static_build_delay.called
 
         # One can direcly unpublish a page, without needing a revision
         self.homepage.unpublish()
-        assert mock_call_command.call_count == 2
-
-        #  check for the call that builds the static site
-        assert mock_call_command.call_args_list[0][0][0] == "build"
-
-        # now check for the call to sync the static site to S3
-        assert mock_call_command.call_args_list[1][0][0] == "publish"
+        assert mock_request_static_build_delay.call_count == 1
