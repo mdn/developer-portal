bbb02a1cda7547c643a2327cc1b5216b2b1ef493
Steve Jalim <steve@somefantastic.co.uk>
2019-10-30 11:48:51 +0000

    Update developerportal/apps/staticbuild/wagtail_hooks.py

--- developerportal/apps/staticbuild/wagtail_hooks.py
+++ developerportal/apps/staticbuild/wagtail_hooks.py
@@ -177,7 +177,7 @@ def _static_build_async(self, force=False, pipeline=settings.STATIC_BUILD_PIPELI
 
 @app.task
 def _publish_scheduled_pages():
-    log_prefix = "[Static-build-and-sync requester]"
+    log_prefix = "[Publish scheduled pages]"
     logger.info(f"{log_prefix} Trying to publish/unpublish scheduled pages")
     call_command("publish_scheduled_pages")
 
