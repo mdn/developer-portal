5fe4e77868ea8911f370b9aff2e9a97948b418d6
limed <limed@mozilla.com>
2019-11-05 17:34:15 -0800

    Cleanup

--- etc/nginx/ssl/README.md
+++ etc/nginx/ssl/README.md
@@ -2,6 +2,6 @@
 
 This folder will contain self-signed certificates for SSL support in local development.
 
-They are generated/updated each time the local Docker stack is upped using `etc/nginx/ssl/generate_dev_cert.sh`
+They are generated/updated each time the local Docker stack is upped using `etc/nginx/ssl/gen-cert.sh`
 
-See `services:app:command` in `docker-compose.yml` for where this is hooked in.
+See `services:cfssl:entrypoint` in `docker-compose.yml` for where this is hooked in.
--- etc/nginx/ssl/gen-cert.sh
+++ etc/nginx/ssl/gen-cert.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
-cfssl gencert -initca ./cfssl.json | cfssljson -bare ca -
-cfssl gencert -ca=./ca.pem -ca-key=./ca-key.pem -config=./ca-config.json -profile=server ./cfssl.json | cfssljson -bare developer-portal-127-0-0-1.nip.io
+SSL_DIR=$(dirname "$0")
+BASENAME="developer-portal-127-0-0-1.nip.io"
+SSL_KEY="${SSL_DIR}/${BASENAME}-key.pem"
+SSL_CRT="${SSL_DIR}/${BASENAME}.pem"
+
+
+if [ ! -f "${SSL_KEY}" ] || [ ! -f "${SSL_CRT}" ]; then
+    cfssl gencert -initca ./cfssl.json | cfssljson -bare ca -
+    cfssl gencert -ca=./ca.pem -ca-key=./ca-key.pem -config=./ca-config.json -profile=server ./cfssl.json | cfssljson -bare "${BASENAME}"
+else
+    echo "${SSL_KEY} and ${SSL_CRT} already exists."
+fi
