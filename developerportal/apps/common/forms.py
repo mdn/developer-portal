49e1fb5a215dcad6d9c4d3e87a8e9dfa78ab72ff
Steve Jalim <steve@somefantastic.co.uk>
2019-11-12 15:38:48 +0000

    726 Surface hidden slug clashes for external content (#735)
    * 726: Show the `slug` field on the Settings page for ExternalContent and its subclasses
    
    This is so that the Wagtail UI will be able to show a problem with a clashing slug, rather
    than have no opportunity to do so and instead raise a 500 when a save is attempted.
    
    * 726: Update dynamic help_text on slug field for External* entities to make it easier to understand

--- developerportal/apps/common/forms.py
+++ developerportal/apps/common/forms.py
@@ -1,17 +1,34 @@
 from wagtail.admin.forms import WagtailAdminPageForm
 from wagtail.core.models import Site
 
+EXTERNAL_ENTITY_NAMES = [
+    # Done as strings to avoid cross-module imports
+    "ExternalArticle",
+    "ExternalContent",
+    "ExternalEvent",
+    "ExternalVideo",
+]
 
-def _custom_slug_help_text():
-    # Generate slug help_text that uses the default site’s real URL, falling
-    # back to example.com instead of the Wagtail default.
-    default_site = Site.objects.filter(is_default_site=True).first()
-    base_url = default_site.root_url if default_site else "https://example.com"
 
-    return (
-        f"The name of the page as it will appear in URLs. For example, "
-        f"for a post: {base_url}/posts/your-slug-here/"
-    )
+def _custom_slug_help_text(model):
+    """Helper to generate entity-appropriate help text without also triggering
+    migration creation (which monkeypatching the models will do)"""
+
+    if model.__class__.__name__ in EXTERNAL_ENTITY_NAMES:
+        return (
+            "Because you are adding External content, this slug will NOT be "
+            "visible to the end user, but still needs to be unique within the CMS."
+        )
+    else:
+        # Generate slug help_text that uses the default site’s real URL, falling
+        # back to example.com instead of the Wagtail default.
+        default_site = Site.objects.filter(is_default_site=True).first()
+        base_url = default_site.root_url if default_site else "https://example.com"
+
+        return (
+            f"The name of the page as it will appear in URLs. For example, "
+            f"for a post: {base_url}/posts/your-slug-here/"
+        )
 
 
 class BasePageForm(WagtailAdminPageForm):
@@ -25,8 +42,7 @@ class BasePageForm(WagtailAdminPageForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
         # Find the default slug field, if possible, and patch it
         slug_field = self.fields.get(self.SLUGFIELD_NAME)
         if slug_field:
-            slug_field.help_text = _custom_slug_help_text()
+            slug_field.help_text = _custom_slug_help_text(self.instance)
