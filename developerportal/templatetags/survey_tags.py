1be657500a2fa33456273570b54a3480c6b87f7c
Steve Jalim <steve@somefantastic.co.uk>
2020-01-27 16:45:54 +0000

    1143: Support obtaining the task-completion survey URL from configuration
    Can be set in an env var, or overridden in production.py

--- developerportal/templatetags/survey_tags.py
+++ developerportal/templatetags/survey_tags.py
@@ -1,8 +1,9 @@
 from django import template
+from django.conf import settings
 
 register = template.Library()
 
 
 @register.inclusion_tag("survey.html")
 def survey_prompt():
-    return {}
+    return {"survey_url": settings.TASK_COMPLETION_SURVEY_URL}
