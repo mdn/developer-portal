081f7bfe05bb195ce04cb7bb3c495e2e77a43ef1
Steve Jalim <steve@somefantastic.co.uk>
2020-01-28 16:39:38 +0000

    1143: Add django-waffle, and use to show survey to 5% of visitors
    Includes data migration to set up that flag

--- developerportal/templatetags/survey_tags.py
+++ developerportal/templatetags/survey_tags.py
@@ -4,6 +4,11 @@ from django.conf import settings
 register = template.Library()
 
 
-@register.inclusion_tag("survey.html")
-def survey_prompt():
-    return {"survey_url": settings.TASK_COMPLETION_SURVEY_URL}
+@register.inclusion_tag("survey.html", takes_context=True)
+def survey_prompt(context):
+    request = context["request"]
+    print("request.COOKIES", request.COOKIES)
+    return {
+        "request": request,  #  Needed by django-waffle in survey.html
+        "survey_url": settings.TASK_COMPLETION_SURVEY_URL,
+    }
