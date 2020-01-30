c2144c3e8bc863a9d3a036068e3ec10512799351
Steve Jalim <steve@somefantastic.co.uk>
2020-01-30 18:10:02 +0000

    1143: Update tests to match changes in previous commit

--- developerportal/apps/common/tests/test_survey_tags.py
+++ developerportal/apps/common/tests/test_survey_tags.py
@@ -19,12 +19,18 @@ class SurveyTagTests(TestCase):
             self.assertNotContains(response, "complete a short survey")
 
     @override_settings(TASK_COMPLETION_SURVEY_URL="https://example.com/test")
-    def test_survey_content_only_rendered_if_flag_allows_it(self):
+    def test_survey_content_always_rendered_regardless_of_flag_state(self):
 
         with override_flag("show_task_completion_survey", active=True):
             response = self.client.get("/")
             self.assertContains(response, "complete a short survey", 1)
 
+            # Check for key CSS/HTML attrs
+            self.assertContains(response, 'js-task-completion-survey" hidden>')
+
         with override_flag("show_task_completion_survey", active=False):
             response = self.client.get("/")
-            self.assertNotContains(response, "complete a short survey")
+            self.assertContains(response, "complete a short survey")
+
+            # Check for key CSS/HTML attrs
+            self.assertContains(response, 'js-task-completion-survey" hidden>')
