2a635801d503430fe893a502fd1836cfea846dd4
Steve Jalim <steve@somefantastic.co.uk>
2020-03-10 21:20:56 +0000

    1207: Add support for styling specified links as buttons in Draftail editor
    (Note: a big hat-tip goes to @kalobtaulien for some his assistance with this, making it a much smaller change than writing an entirely new Draftail Entity.)
    
    This changeset supports a way of marking certain links in a rich-text area as buttons, so that they are rendered as such in the published page.
    
    (This should allow us to make pages that feature lots of buttons without the page becoming too heavy/complex, which appears to be a factor in editing hanging after a certain tipping point.)
    
    It acheives this by adding a new button to the Draftail editor which adds a new block (a div, lightly styled with a dashed grey border) inside which one can add hyperlinks using rich-text editor's standard link button. Any links inside this block will be rendered as buttons in the published (and previewed) page, and are lightly styled to look similar within the rich-text editor itself.

--- developerportal/apps/common/wagtail_hooks.py
+++ developerportal/apps/common/wagtail_hooks.py
@@ -1,6 +1,9 @@
 # pylint: disable=no-member
-from django.utils.html import escape
+from django.contrib.staticfiles.templatetags.staticfiles import static
+from django.utils.html import escape, format_html
 
+import wagtail.admin.rich_text.editors.draftail.features as draftail_features
+from wagtail.admin.rich_text.converters.html_to_contentstate import BlockElementHandler
 from wagtail.core import hooks
 from wagtail.core.rich_text import LinkHandler
 
@@ -24,3 +27,42 @@ class NewWindowExternalLinkHandler(LinkHandler):
 @hooks.register("register_rich_text_features")
 def register_external_link(features):
     features.register_link_type(NewWindowExternalLinkHandler)
+
+
+@hooks.register("register_rich_text_features")
+def register_button_section_feature(features):
+    """Support marking a block of links as buttons within Draftail."""
+    feature_name = "button-block"
+    type_ = "button-block"
+    tag = "div"
+    control = {
+        "type": type_,
+        "label": "",
+        "description": "Add a block will that render any links inside it as buttons",
+        "element": "div",
+        "icon": "icon icon-placeholder",
+    }
+    features.register_editor_plugin(
+        "draftail", feature_name, draftail_features.BlockFeature(control)
+    )
+    features.register_converter_rule(
+        "contentstate",
+        feature_name,
+        {
+            "from_database_format": {tag: BlockElementHandler(type_)},
+            "to_database_format": {
+                "block_map": {
+                    type_: {"element": "div", "props": {"class": "links-as-buttons"}}
+                }
+            },
+        },
+    )
+    features.default_features.append("button-block")
+
+
+@hooks.register("insert_global_admin_css", order=100)
+def global_admin_css():
+    """Slot in custom CMS CSS to render the above button-block element nicely."""
+    return format_html(
+        '<link rel="stylesheet" href="{}">', static("css/admin_extras.css")
+    )
