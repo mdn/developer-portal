f054338800bc6c4adff7e80afd4bf3ea4ffb5af7
Steve Jalim <steve@somefantastic.co.uk>
2020-06-01 11:12:55 +0100

    1510: Make "Posts"-page content indexable for Wagtail search (#1512)
    * 1510: Enable Wagtail search indexing of models which populate the Posts page
    
    Using the Postgres backend (which is recommended for sites with less than 1m pages to index), we are now indexing
    
    * Article
    * Video
    * ExternalArticle
    * ExternalVideo
    
    Note that migrations need to be run to add the DB tables for the postgres-backed search
    
    Example of this working in the shell:
    
    >>> from developerportal.apps.articles.models import *
    >>> from developerportal.apps.videos.models import *
    >>> from developerportal.apps.externalcontent.models import *
    >>> Article.objects.search("Firefox 66")
    <SearchResults [<Article: Scroll Anchoring in Firefox 66>, <Article: Firefox 66: The Sound of Silence>]>
    >>> ExternalArticle.objects.search("Firefox 66")
    <SearchResults []>
    >>> ExternalArticle.objects.search("Firefox")
    <SearchResults [<ExternalArticle: Firefox Reality for HoloLens 2>, <ExternalArticle: Firefox Reality 10>, <ExternalArticle: High Performance Web Audio with AudioWorklet in Firefox>, <ExternalArticle: Firefox 76: Audio worklets and other tricks>, <ExternalArticle: Extensions in Firefox 76>, <ExternalArticle: Better User Performance and More in this Firefox Reality v9 release>, <ExternalArticle: Firefox 75: Ambitions for April>, <ExternalArticle: Engineering code quality in the Firefox browser: A look at our tools and challenges>, <ExternalArticle: A Taste of WebGPU in Firefox>, <ExternalArticle: What to expect for the upcoming deprecation of FTP in Firefox>, <ExternalArticle: April Extensions for Firefox Preview>, <ExternalArticle: Twitter Direct Message Caching and Firefox>, <ExternalArticle: Innovating on Web Monetization: Coil and Firefox Reality>, <ExternalArticle: Security means more with Firefox 74>, <ExternalArticle: Firefox 73 is upon us>, <ExternalArticle: Securing Firefox with WebAssembly>, <ExternalArticle: Future-proofing Firefox’s JavaScript Debugger Implementation>, <ExternalArticle: Extensions in Firefox 73>, <ExternalArticle: FAQ for extension support in new Firefox for Android>, <ExternalArticle: Extensions in Firefox 74>, '...(remaining elements truncated)...']>
    >>> Video.objects.search("Firefox")
    <SearchResults [<Video: Max Liu - Learn more about Firefox Lite - Mozilla Developer Roadshow - Taipei (Chinese)>, <Video: Why are there Four Firefoxes?>]>
    >>> ExternalVideo.objects.search("Firefox")
    <SearchResults []>
    >>> ExternalVideo.objects.all()
    <PageQuerySet [<ExternalVideo: test video 1>, <ExternalVideo: test video 2>, <ExternalVideo: test external video>]>
    >>> ExternalVideo.objects.search("external")
    <SearchResults [<ExternalVideo: test external video>]>
    >>> ExternalVideo.objects.search("test 2")
    <SearchResults [<ExternalVideo: test video 2>]>
    >>> ExternalVideo.objects.search("test 1")
    <SearchResults [<ExternalVideo: test video 1>]>
    
    * 1510: Add a scheduled task to rebuild the search index once a week
    
    While we also automatically and atomically update the search index when a Page is saved, we rebuild the whole thing weekly. This is in line with Wagtail's own recommendations: https://docs.wagtail.io/en/stable/topics/search/indexing.html#the-update-index-command
    
    Note that we don't need to trigger it after the feed importers, because those processes save Pages, which will auto-trigger re-indexing. HOWEVER, we may need to back off that automatic updating if it looks like it's saturating the server when importing lots of items -- though the change of that is currently pretty small, more like a couple of items a day at most, because it's coupled to additions to actual external blogs/channels etc

--- developerportal/apps/articles/models.py
+++ developerportal/apps/articles/models.py
@@ -29,6 +29,7 @@ from wagtail.core.blocks import PageChooserBlock
 from wagtail.core.fields import RichTextField, StreamBlock, StreamField
 from wagtail.core.models import Orderable
 from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.search import index
 
 from ..common.blocks import ExternalAuthorBlock, ExternalLinkBlock
 from ..common.constants import (
@@ -305,6 +306,12 @@ class Article(BasePage):
             ObjectList(settings_panels, heading="Settings", classname="settings"),
         ]
     )
+
+    # Search config
+    search_fields = BasePage.search_fields + [  # Inherit search_fields from Page
+        index.SearchField("description")
+        # "title" is already specced in BasePage
+    ]
 
     def get_absolute_url(self):
         # For the RSS feed
