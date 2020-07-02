82931b02a169b04b40799496f59cd8e114c67ba4
Steve Jalim <steve@somefantastic.co.uk>
2020-07-02 11:41:23 +0100

    1605: Tweak the model and last migration to allow more than two featured items for the Events page
    It's not a change that affects the database schema, so it's OK to tweak the most recent migration

--- developerportal/apps/events/migrations/0025_update_streamblock.py
+++ developerportal/apps/events/migrations/0025_update_streamblock.py
@@ -8,19 +8,98 @@ import wagtail.images.blocks
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('events', '0024_add_3_2_ratio_image'),
-    ]
+    dependencies = [("events", "0024_add_3_2_ratio_image")]
 
     operations = [
         migrations.AlterField(
-            model_name='event',
-            name='speakers',
-            field=wagtail.core.fields.StreamField([('speaker', wagtail.core.blocks.PageChooserBlock(page_type=['people.Person'])), ('external_speaker', wagtail.core.blocks.StructBlock([('title', wagtail.core.blocks.CharBlock(label='Name')), ('job_title', wagtail.core.blocks.CharBlock()), ('image', wagtail.images.blocks.ImageChooserBlock(help_text='16:9 aspect-ratio image', label='16:9 image')), ('image_3_2', wagtail.images.blocks.ImageChooserBlock(help_text='3:2 aspect-ratio image - optional but recommended', required=False)), ('url', wagtail.core.blocks.URLBlock(label='URL', required=False))]))], blank=True, help_text='Optional list of speakers for this event', null=True),
+            model_name="event",
+            name="speakers",
+            field=wagtail.core.fields.StreamField(
+                [
+                    (
+                        "speaker",
+                        wagtail.core.blocks.PageChooserBlock(
+                            page_type=["people.Person"]
+                        ),
+                    ),
+                    (
+                        "external_speaker",
+                        wagtail.core.blocks.StructBlock(
+                            [
+                                ("title", wagtail.core.blocks.CharBlock(label="Name")),
+                                ("job_title", wagtail.core.blocks.CharBlock()),
+                                (
+                                    "image",
+                                    wagtail.images.blocks.ImageChooserBlock(
+                                        help_text="16:9 aspect-ratio image",
+                                        label="16:9 image",
+                                    ),
+                                ),
+                                (
+                                    "image_3_2",
+                                    wagtail.images.blocks.ImageChooserBlock(
+                                        help_text="3:2 aspect-ratio image - optional but recommended",
+                                        required=False,
+                                    ),
+                                ),
+                                (
+                                    "url",
+                                    wagtail.core.blocks.URLBlock(
+                                        label="URL", required=False
+                                    ),
+                                ),
+                            ]
+                        ),
+                    ),
+                ],
+                blank=True,
+                help_text="Optional list of speakers for this event",
+                null=True,
+            ),
         ),
         migrations.AlterField(
-            model_name='events',
-            name='featured',
-            field=wagtail.core.fields.StreamField([('event', wagtail.core.blocks.PageChooserBlock(page_type=['events.Event', 'externalcontent.ExternalEvent'])), ('external_page', wagtail.core.blocks.StructBlock([('url', wagtail.core.blocks.URLBlock()), ('title', wagtail.core.blocks.CharBlock()), ('description', wagtail.core.blocks.TextBlock(required=False)), ('image', wagtail.images.blocks.ImageChooserBlock(help_text='16:9 aspect-ratio image', label='16:9 image')), ('image_3_2', wagtail.images.blocks.ImageChooserBlock(help_text='3:2 aspect-ratio image - optiopnal but recommended', label='3:2 image', required=False))]))], blank=True, help_text='Optional space to show featured events. Note that these are rendered two-up, so please set 0 or 2', null=True),
+            model_name="events",
+            name="featured",
+            field=wagtail.core.fields.StreamField(
+                [
+                    (
+                        "event",
+                        wagtail.core.blocks.PageChooserBlock(
+                            page_type=["events.Event", "externalcontent.ExternalEvent"]
+                        ),
+                    ),
+                    (
+                        "external_page",
+                        wagtail.core.blocks.StructBlock(
+                            [
+                                ("url", wagtail.core.blocks.URLBlock()),
+                                ("title", wagtail.core.blocks.CharBlock()),
+                                (
+                                    "description",
+                                    wagtail.core.blocks.TextBlock(required=False),
+                                ),
+                                (
+                                    "image",
+                                    wagtail.images.blocks.ImageChooserBlock(
+                                        help_text="16:9 aspect-ratio image",
+                                        label="16:9 image",
+                                    ),
+                                ),
+                                (
+                                    "image_3_2",
+                                    wagtail.images.blocks.ImageChooserBlock(
+                                        help_text="3:2 aspect-ratio image - optiopnal but recommended",
+                                        label="3:2 image",
+                                        required=False,
+                                    ),
+                                ),
+                            ]
+                        ),
+                    ),
+                ],
+                blank=True,
+                help_text="Optional space to show featured events.",
+                null=True,
+            ),
         ),
     ]
