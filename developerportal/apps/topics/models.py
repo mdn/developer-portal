e2e8d3236b59a6d7ba75095c6b8cf55c6e33ffe8
Steve Jalim <steve@somefantastic.co.uk>
2020-07-09 15:00:32 +0100

    1392: Add "related events" section to Topic pages, using banded-background styling (#1663)
    * 1392: Modelling update to add Topic.related_events StreamField
    
    This is so we can explicitly link to relevant Events, rather than using the Topic.events property, which gets the most recent Events, which isn't always what we want.
    
    * 1392: Support manually specifying whether the four-card-row organism has a tinted background or not
    
    (and then ensuring it still does in the Events page's Featured panel)
    
    * 1392: Support an optional tinted background on the People organism
    
    * 1392: Add Topic.get_section_background_panel_hints helper to provide opinionated config to the template for whether a panel should or should not have a tinted background
    
    * 1392: Support alternate tinted/not-tinted banding styling on Topics page
    
    Uses the "tint hints" helper method in the Topic page template to pass configuration into the organisms it uses, if they are present
    
    * 1252: Support extra content on event page (#1667)
    
    * chore(deps): update dependency boto3 to v1.14.17 (#1668)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * 1252: Add modelling and wagtail admin updates to support showing extra page links from an Event page
    
    - [X] Add "Extra content" streamfield to Event page, linking to Video, Article and Topic pages
    - [X] Add custom header field for "Extra content"
    - [X] Admin config for this
    
    * 1252: Show the extra content in the Event page, in a four-in-a-row unit
    
    * 1252: Support a streamfield of content below the event card in the right-side sidebar
    
    This is so we can add a button off to supplementary content pages, and/or add extra info as required
    
    * 1252: Fix up migration ordering to deal with cross-branch conflicts
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * Update developerportal/apps/topics/tests/test_topics.py
    
    * Update developerportal/apps/topics/tests/test_topics.py
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Renovate Bot <bot@renovateapp.com>

--- developerportal/apps/topics/models.py
+++ developerportal/apps/topics/models.py
@@ -29,12 +29,7 @@ from wagtail.images.edit_handlers import ImageChooserPanel
 from ..common.blocks import FeaturedExternalBlock
 from ..common.constants import RICH_TEXT_FEATURES_SIMPLE
 from ..common.models import BasePage
-from ..common.utils import (
-    get_combined_articles,
-    get_combined_events,
-    get_combined_videos,
-    get_past_event_cutoff,
-)
+from ..common.utils import get_combined_articles, get_combined_videos
 from ..common.validators import check_for_svg_file
 
 
@@ -131,6 +126,25 @@ class Topic(BasePage):
         ),
     )
 
+    # "Relevant Events" panel
+    relevant_events = StreamField(
+        StreamBlock(
+            [
+                (
+                    "event",
+                    PageChooserBlock(
+                        target_model=("events.Event", "externalcontent.ExternalEvent")
+                    ),
+                )
+            ],
+            max_num=4,
+            required=False,
+        ),
+        null=True,
+        blank=True,
+        help_text=("Optional space for featured Events, max. 4."),
+    )
+
     # Card fields
     card_title = CharField("Title", max_length=140, blank=True, default="")
     card_description = TextField("Description", max_length=400, blank=True, default="")
@@ -165,6 +179,7 @@ class Topic(BasePage):
         FieldPanel("description"),
         StreamFieldPanel("featured"),
         StreamFieldPanel("recent_work"),
+        StreamFieldPanel("relevant_events"),
         MultiFieldPanel(
             [InlinePanel("people")],
             heading="Content by",
@@ -257,15 +272,6 @@ class Topic(BasePage):
     def articles(self):
         return get_combined_articles(self, topics__topic__pk=self.pk)
 
-    @property
-    def events(self):
-        """Return upcoming events for this topic,
-        ignoring events in the past, ordered by start date"""
-        return get_combined_events(
-            self, topics__topic__pk=self.pk, start_date__gte=get_past_event_cutoff()
-        )
-
-    @property
     def experts(self):
         """Return Person instances for topic experts"""
         return [person.person for person in self.people.all()]
@@ -278,6 +284,38 @@ class Topic(BasePage):
     @property
     def subtopics(self):
         return [topic.child for topic in self.child_topics.all()]
+
+    def get_section_background_panel_hints(self) -> dict:
+        """When we have multiple optional sections on a Topic page, currently comprising
+        'recent_work', 'relevant_events' and 'experts', we want to ensure they are
+        rendered on contrasting panels.
+
+        This method (which is opinionated based on state, hence a method, not a
+        function) returns a dictionary referencing the name of each panel and whether
+        it should be on a tinted panel.
+        """
+        output = {
+            # Default state
+            "recent_work": False,
+            "relevant_events": False,
+            "experts": False,
+        }
+
+        if self.recent_work:
+            # If present, always takes a tint panel
+            output["recent_work"] = True
+
+        if self.relevant_events:
+            output["relevant_events"] = not self.recent_work
+
+        if self.experts:
+            if not self.recent_work and not self.relevant_events:
+                output["experts"] = True  # Only panel, so tint it
+            elif self.recent_work and self.relevant_events:
+                output["experts"] = True  # Third panel, so tint it
+            else:
+                output["experts"] = False
+        return output
 
 
 class Topics(BasePage):
